93c46cf6b4bf6081135434800a0d0ed3b5784b0a
liaohui-2002 <lh603143764@163.com>
2022-12-30 14:27:45 +0800

    一人一单秒杀问题  悲观锁

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -45,14 +45,15 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
 
         //缓存穿透
-        Shop shop = cacheClient.
-                queryWithPassThrough(CACHE_SHOP_KEY,id,Shop.class,this::getById,CACHE_SHOP_TTL,TimeUnit.MINUTES);
+        //Shop shop = cacheClient.
+          //      queryWithPassThrough(CACHE_SHOP_KEY,id,Shop.class,this::getById,CACHE_SHOP_TTL,TimeUnit.MINUTES);
 
         //互斥锁解决缓存击穿问题
-        //Shop shop = queryWithMutex(id);
+        Shop shop = queryWithMutex(id);
 
         //逻辑过期解决缓存击穿问题
-        //Shop shop = queryWithLogicalExpire(id);
+//        Shop shop = cacheClient.
+//                queryWithLogicalExpire(CACHE_SHOP_KEY,id,Shop.class,this::getById,20L,TimeUnit.SECONDS);
         if(shop == null){
             return  Result.fail("店铺不存在！");
         }
